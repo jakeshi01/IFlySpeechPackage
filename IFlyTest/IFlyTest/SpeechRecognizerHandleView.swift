9786613442720cb1d4c717cb665c1aeda3479ca6
jakeshi <jakeshi@meijiabang.cn>
2017-07-02 17:41:11 +0800

    添加SpeechRecognizerHandleView

---
+++ IFlyTest/IFlyTest/SpeechRecognizerHandleView.swift
@@ -0,0 +1,14 @@
+//
+//  SpeechRecognizerHandleView.swift
+//  IFlyTest
+//
+//  Created by 石 苏杰 on 2017/7/2.
+//  Copyright © 2017年 Jake. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SpeechRecognizerHandleView: UIView {
+    
+}
